3e436d8fab29b967a6039f749c9b76658bf32ee5
sadatowrustam <rustamsadatov0@gmail.com>
2024-06-21 16:41:43 +0500

    ds1

--- routes/public/routes/footerRouter.js
+++ routes/public/routes/footerRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {getAllBlogs} = require('../../../controllers/admin/projectsControllers');
+const {getAllBlogs} = require('../../../controllers/admin/footerControllers');
 const { getBlog }=require("../../../controllers/public/footerController")
 const router = express.Router();
 router.get("/",getAllBlogs)
