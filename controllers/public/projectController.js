d4533a42a96938891072708c8c3992f342bdcfba
sadatowrustam <rustamsadatov0@gmail.com>
2024-09-11 13:07:12 +0500

    update

--- controllers/public/projectController.js
+++ controllers/public/projectController.js
@@ -1,10 +1,12 @@
+const AppError = require("../../utils/appError");
+const catchAsync = require("../../utils/catchAsync");
+const { Footer, Images, Projects } = require("../../models");
 
-const AppError = require('../../utils/appError');
-const catchAsync = require('../../utils/catchAsync');
-const { Footer,Images} = require('../../models');
-
-exports.getBlog=catchAsync(async(req,res,next)=>{
-    const blogs=await Footer.findOne({where:{id:req.params.id},include:{model:Images,as:"images"}})
-    if(!blogs) return next(new AppError("Footer not found",404))
-    return res.send(blogs)
-})
+exports.getBlog = catchAsync(async (req, res, next) => {
+  const blogs = await Projects.findOne({
+    where: { id: req.params.id },
+    include: { model: Images, as: "images" },
+  });
+  if (!blogs) return next(new AppError("Footer not found", 404));
+  return res.send(blogs);
+});
