c31565ecf13d08a80ecb95c3dfd67849e25f5e85
sadatowrustam <rustamsadatov0@gmail.com>
2024-09-12 13:03:44 +0500

    update

--- controllers/public/projectController.js
+++ controllers/public/projectController.js
@@ -1,12 +1,12 @@
 const AppError = require("../../utils/appError");
 const catchAsync = require("../../utils/catchAsync");
-const { Footer, Images, Projects } = require("../../models");
+const { Images, Projects } = require("../../models");
 
 exports.getBlog = catchAsync(async (req, res, next) => {
   const blogs = await Projects.findOne({
     where: { id: req.params.id },
     include: { model: Images, as: "images" },
   });
-  if (!blogs) return next(new AppError("Footer not found", 404));
+  if (!blogs) return next(new AppError("Project not found", 404));
   return res.send(blogs);
 });
--- routes/public/routes/projectsRouter.js
+++ routes/public/routes/projectsRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const {getAllBlogs} = require('../../../controllers/admin/projectsControllers');
-const { getBlog }=require("../../../controllers/public/footerController")
+const { getBlog }=require("../../../controllers/public/projectsController")
 const router = express.Router();
 router.get("/",getAllBlogs)
 router.get("/:id",getBlog )
